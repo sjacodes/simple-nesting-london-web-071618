06b66bd706d1e1a6a0bba192d047fb63d588352b
Sarah Jacob <smjacob91@gmail.com>
2018-07-07 11:45:03 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -37,9 +37,6 @@ def alan_kay_is_known_for
         }
      }
      return programmer_hash[:alan_kay][:known_for]
-     
-     # What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
 end
 
 def dennis_ritchies_language
