7b39054a9b7dd96e82e9741af040e9ac098721ee
Sarah Jacob <smjacob91@gmail.com>
2018-07-07 12:07:22 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -125,3 +125,21 @@ def adding_to_dennis
      }
 
 end
+
+  describe "#adding_matz" do 
+    it "operates on the programmer_hash and adds a key/value pair to the top level of the hash, returning the newly-added-to hash" do 
+      expect(adding_matz.keys).to include(:yukihiro_matsumoto)
+    end
+  end
+
+  describe "#changing_alan" do 
+    it "operates on the programmer_hash and changes what Alan Kay is known for, returning the newly-changed hash" do 
+      expect(changing_alan[:alan_kay][:known_for]).to eq("GUI")
+    end
+  end
+
+  describe "#adding_to_dennis" do 
+    it "operates on the programmer_hash and adds 'Assembly' to Dennis Ritchie's languages, returning the newly-added-to-hash" do 
+      expect(adding_to_dennis[:dennis_ritchie][:languages][1]).to include("Assembly")
+    end
+  end
