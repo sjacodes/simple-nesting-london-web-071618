ce29abe21067fcb80da1f3c52cb8c26c328c2cd2
Sarah Jacob <smjacob91@gmail.com>
2018-07-07 12:17:42 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -125,9 +125,5 @@ def adding_to_dennis
         }
      }
      programmer_hash[:dennis_ritchie][:languages].push("Assembly")
-     return 
-
+     return programmer_hash
 end
-
- it "operates on the programmer_hash and adds 'Assembly' to Dennis Ritchie's languages, returning the newly-added-to-hash" 
- 
