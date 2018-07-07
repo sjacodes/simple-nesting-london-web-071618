f45e2547d067137bf3e4b1ca6e63dbaa4499c4be
Sarah Jacob <smjacob91@gmail.com>
2018-07-07 11:45:50 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -39,6 +39,12 @@ def alan_kay_is_known_for
      return programmer_hash[:alan_kay][:known_for]
 end
 
+  describe "#dennis_ritchies_language" do 
+    it "operates on the programmer_hash and returns the value of Dennis Ritchie's language as a string" do 
+      expect(dennis_ritchies_language).to eq("C")
+    end
+  end
+  
 def dennis_ritchies_language
 	programmer_hash = 
  		{
@@ -55,6 +61,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     return 
 end
 
 def adding_matz
